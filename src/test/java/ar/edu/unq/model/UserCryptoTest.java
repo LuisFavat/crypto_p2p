c3b61005c163dcc2f7c192de25fd411e63c386a4
LuisFavat <73365264+LuisFavat@users.noreply.github.com>
2023-08-28 22:55:05 -0300

    "testing actions"

---
+++ src/test/java/ar/edu/unq/model/UserCryptoTest.java
@@ -0,0 +1,17 @@
+package ar.edu.unq.model;
+import ar.edu.unq.cryptop2p.model.UserCrypto;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+
+
+public class UserCryptoTest {
+    	@Test
+	void SePuedeInstanciarUnUsuario() {
+        var user = new UserCrypto(0L, "Jean", "Valjean", "Plaza de los Vosgos nro 6", "lesmiserables@google.com", "1234567890123456789012", "123456");
+
+        assertEquals("a","b");
+	}
+}
