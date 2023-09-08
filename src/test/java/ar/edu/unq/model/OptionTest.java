801d501d2fe4c17676c01ea355ed67898e0d6f54
AlexEnigmaCode <alegfprogramador@gmail.com>
2023-09-08 20:40:43 -0300

    metodos para validar el rango permitido segun cotizacion

--- src/test/java/ar/edu/unq/model/OptionTest.java
+++ src/test/java/ar/edu/unq/model/OptionTest.java
@@ -1,13 +1,9 @@
 package ar.edu.unq.model;
 
-import ar.edu.unq.cryptop2p.model.Option;
-import ar.edu.unq.cryptop2p.model.OptionCall;
+
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 import static ar.edu.unq.model.builders.OptionCallBuilder.*;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
