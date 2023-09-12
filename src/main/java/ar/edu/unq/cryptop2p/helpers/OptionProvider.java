dca19352822e6d9d4ecd0e050c52a7631a002481
AlexEnigmaCode <alegfprogramador@gmail.com>
2023-09-12 15:00:11 -0300

    Refactor para proveer la intencion correspondiente

--- src/main/java/ar/edu/unq/cryptop2p/helpers/OptionProvider.java
+++ src/main/java/ar/edu/unq/cryptop2p/helpers/OptionProvider.java
@@ -1,38 +1,18 @@
 package ar.edu.unq.cryptop2p.helpers;
 
-import ar.edu.unq.cryptop2p.builders.OptionBuilder;
-import ar.edu.unq.cryptop2p.builders.OptionCallBuilder;
-import ar.edu.unq.cryptop2p.builders.OptionPutBuilder;
 import ar.edu.unq.cryptop2p.model.Option;
-import ar.edu.unq.cryptop2p.model.OptionCall;
-import ar.edu.unq.cryptop2p.model.OptionPut;
 import ar.edu.unq.cryptop2p.model.dto.OptionPostDTO;
-import jakarta.annotation.PostConstruct;
-
-import java.util.HashSet;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Set;
+import org.jetbrains.annotations.NotNull;
 
 public class OptionProvider {
-
-    private final LinkedList<OptionBuilder>options = new LinkedList<>();
-
-    @PostConstruct
-    private  void init(){
-       options.add( OptionCallBuilder.anOption() );
-       options.add( OptionPutBuilder.anOption() );
-
-    }
-
-   public  Option provide (OptionPostDTO optionDTO){
-    OptionBuilder amOption =  options.stream().filter(  option -> option.getTyoe() == optionDTO.getType()  ).findFirst().get();
-   return  amOption
-           .withUser(optionDTO.getUser())
-           .withCryptoCurrency(optionDTO.getCryptocurrency())
-           .withPrice(optionDTO.getPrice())
-           .withCryptoAmount(optionDTO.getCryptoAmount())
-           .build();
+    @NotNull
+   public static  Option provide (@NotNull OptionPostDTO optionDTO){
+     var  anOption =   optionDTO.getType().getOption();
+     anOption.setPrice(optionDTO.getPrice());
+     anOption.setUser(optionDTO.getUser());;
+     anOption.setCryptocurrency(optionDTO.getCryptocurrency());
+     anOption.setCryptoAmount(optionDTO.getCryptoAmount());
+     return anOption;
 
     }
 }
--- src/main/java/ar/edu/unq/cryptop2p/helpers/OptionType.java
+++ src/main/java/ar/edu/unq/cryptop2p/helpers/OptionType.java
@@ -1,6 +1,18 @@
 package ar.edu.unq.cryptop2p.helpers;
 
-public enum OptionType {
-   OPTIONCALL,
-   OPTIONPUT
+import ar.edu.unq.cryptop2p.model.*;
+import lombok.Getter;
+
+@Getter
+
+public enum  OptionType {
+   OPTIONCALL (new OptionCall() ) ,
+   OPTIONPUT (new OptionPut()) ;
+
+   private Option option;
+
+  private  OptionType ( Option option  ) {
+
+   }
+
 }
