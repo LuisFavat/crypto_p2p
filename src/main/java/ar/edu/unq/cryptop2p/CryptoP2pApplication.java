3657972c12f96de24604f61b558443962f68aa19
LuisFavat <73365264+LuisFavat@users.noreply.github.com>
2023-09-19 11:08:53 -0300

    Se elimino workflow (gradle_.yml) innecesario

--- src/main/java/ar/edu/unq/cryptop2p/CryptoP2pApplication.java
+++ src/main/java/ar/edu/unq/cryptop2p/CryptoP2pApplication.java
@@ -3,8 +3,6 @@ package ar.edu.unq.cryptop2p;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
-
 @SpringBootApplication
 public class CryptoP2pApplication {
 
