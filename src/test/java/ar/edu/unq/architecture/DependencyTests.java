177cf110169bc30553015dcb958eafada8ba7c6e
LuisFavat <73365264+LuisFavat@users.noreply.github.com>
2023-11-09 19:26:04 -0300

    fix

--- src/test/java/ar/edu/unq/architecture/DependencyTests.java
+++ src/test/java/ar/edu/unq/architecture/DependencyTests.java
@@ -3,9 +3,12 @@ package ar.edu.unq.architecture;
 import com.tngtech.archunit.core.domain.JavaClasses;
 import com.tngtech.archunit.core.importer.ClassFileImporter;
 import com.tngtech.archunit.core.importer.ImportOption;
+import com.tngtech.archunit.lang.syntax.ArchRuleDefinition;
+import lombok.val;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
 import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.noClasses;
 
 public class DependencyTests
@@ -22,8 +25,20 @@ public class DependencyTests
     @Test
     public void ServiceNotDependOnWebServices()
     {
-        noClasses().that().resideInAPackage("..service..")
+        val rule = ArchRuleDefinition.noClasses().that().resideInAPackage("..service..")
                 .should().dependOnClassesThat().resideInAPackage("..webservice..");
+        rule.check(baseClasses);
     }
-    
+
+    @Test
+    public void webserviceOnlyDependOnService()
+    {
+        val rule = ArchRuleDefinition.classes().that().resideInAPackage("..webservice..")
+                .should().onlyHaveDependentClassesThat().resideInAnyPackage("..service..");
+        rule.check(baseClasses);
+    }
+
+
+
+
 }
