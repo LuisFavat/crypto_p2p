579cbcb4794482304f064e1b93120fb2619c9b70
LuisFavat <73365264+LuisFavat@users.noreply.github.com>
2023-11-09 15:39:54 -0300

    aspects

---
+++ src/main/java/ar/edu/unq/cryptop2p/audition/Duration.java
@@ -0,0 +1,25 @@
+package ar.edu.unq.cryptop2p.audition;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+@Order(3)
+public class Duration {
+
+    @Around("ar.edu.unq.cryptop2p.audition.PointCuts.entryPoint()")
+    public void durationTime(ProceedingJoinPoint joinPoint) throws Throwable {
+        long start = System.currentTimeMillis();
+
+        Object proceed = joinPoint.proceed();
+
+        long end = System.currentTimeMillis();
+
+        PointCuts.logger.info("Duration: " + (end - start));
+        System.out.println("end ----- around");
+    }
+}
