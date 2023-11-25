309d13412bcd0e99b1fbc9b46df8dd591882c11d
LuisFavat <73365264+LuisFavat@users.noreply.github.com>
2023-11-25 17:31:40 -0300

    update

--- src/main/java/ar/edu/unq/cryptop2p/cache/RedisConfig.java
+++ src/main/java/ar/edu/unq/cryptop2p/cache/RedisConfig.java
@@ -1,10 +1,12 @@
 package ar.edu.unq.cryptop2p.cache;
 
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 
+@EnableCaching
 @Configuration
 public class RedisConfig {
 
