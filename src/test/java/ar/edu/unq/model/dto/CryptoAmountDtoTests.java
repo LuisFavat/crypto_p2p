9995baf25573e4a552e5860fcd97f42e01bc7b39
LuisFavat <73365264+LuisFavat@users.noreply.github.com>
2023-11-28 15:46:11 -0300

    update

--- src/test/java/ar/edu/unq/model/dto/CryptoAmountDtoTests.java
+++ src/test/java/ar/edu/unq/model/dto/CryptoAmountDtoTests.java
@@ -9,11 +9,36 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class CryptoAmountDtoTests {
 
     @Test
-    public void contructorTest()
+    public void fullContructorTest()
     {
         var amount = 1;
         var name = "cryptoName";
         var cryptoAmountDTO = new CryptoAmountDto(amount, name);
+
+        assertEquals(amount, cryptoAmountDTO.getAmount());
+        assertEquals(name, cryptoAmountDTO.getCryptoName());
+    }
+
+    @Test
+    public void emptyContructorTest()
+    {
+        var amount = 0;
+        String name = null;
+        var cryptoAmountDTO = new CryptoAmountDto();
+
+        assertEquals(amount, cryptoAmountDTO.getAmount());
+        assertEquals(name, cryptoAmountDTO.getCryptoName());
+    }
+
+    @Test
+    public void setterTest()
+    {
+        var amount = 1;
+        String name = "aName";
+
+        var cryptoAmountDTO = new CryptoAmountDto();
+        cryptoAmountDTO.setAmount(amount);
+        cryptoAmountDTO.setCryptoName(name);
 
         assertEquals(amount, cryptoAmountDTO.getAmount());
         assertEquals(name, cryptoAmountDTO.getCryptoName());
